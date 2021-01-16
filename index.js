5d55c24523ebc46557227ad8b15beebf300330f5
Aytuğ Ulış <aytugulis@gmail.com>
2021-01-16 22:53:01 +0300

    Checked words and styles

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// CONFIG
 const config = {
   type: Phaser.AUTO,
   parent: 'phaser-example',
@@ -17,31 +18,34 @@ const config = {
 
 const game = new Phaser.Game(config)
 
+// INITIAL
 function init() {
-  this.score = 0,
-  this.drag = false,
-  this.secretWords = ['FEAR', 'FEEL', 'LET', 'TEAR', 'RED'],
-  this.count = this.secretWords.length,
-  this.wordList = [],
-  this.finalWordList = [],
-  this.finalWord = '',
-  this.totalWord = 5,
-  this.style = {
-    'color': 'blue',
-    'font': '35px Arial',
-    'font-weight': 'bold'
-}
+    (this.score = 0),
+    (this.drag = false),
+    (this.secretWords = ['FEAR', 'FEEL', 'LET', 'TEAR', 'RED']),
+    (this.count = this.secretWords.length),
+    (this.wordList = []),
+    (this.finalWordList = []),
+    (this.finalWord = ''),
+    (this.totalWord = 5),
+    (this.style = {
+      'color': 'blue',
+      'font': '35px Arial',
+      'font-weight': 'bold',
+      'cursor': 'context-menu'
+    })
 }
 
+//PRELOAD
 function preload() {
-  this.load.html('text', 'tex/text.html');
-  this.load.image('background', 'tex/bg.png')
-  this.load.image('box', 'tex/box.jpg')
-  this.load.image('box-a', 'tex/box-a.jpg')
+  this.load.html('text', 'src/text.html')
+  this.load.image('background', 'texture/bg.png')
+  this.load.image('box', 'texture/box.jpg')
+  this.load.image('box-a', 'texture/box-a.jpg')
 }
 
+//CREATE
 function create() {
-
   // CREATE BACKGROUND
   const bg = this.add.sprite(0, 0, 'background')
   bg.setOrigin(0, 0)
@@ -49,7 +53,6 @@ function create() {
   // CREATE SCORE TEXT
   this.scoreText = this.add.text(100, 16, 'score: ' + this.score, {
     fontSize: '24px',
-    fill: '#fff',
   })
 
   // CREATE BOXES
@@ -69,25 +72,25 @@ function create() {
   this.box14 = this.add.sprite(412, 358, 'box')
 
   // TEXT AREA
-
-  this.text = this.add.dom(600, 300).createFromCache('text');
-  this.text.addListener('mousedown');
-  this.text.addListener('mousemove');
-  this.text.addListener('mouseup');
+  this.text = this.add.dom(600, 300).createFromCache('text')
+  this.text.addListener('mousedown')
+  this.text.addListener('mousemove')
+  this.text.addListener('mouseup')
 
   this.text.on('mousedown', (e) => {
     this.drag = true
   })
 
   this.text.on('mousemove', (e) => {
-    if(this.drag){
-      if(e.target.value !== undefined && this.wordList.includes(e.target.value) === false){
+    if (this.drag) {
+      if (
+        e.target.value !== undefined &&
+        this.wordList.includes(e.target.value) === false
+      ) {
         this.wordList.push(e.target.value)
-
       }
     }
   })
-
 
   this.text.on('mouseup', (e) => {
     this.drag = false
@@ -97,38 +100,69 @@ function create() {
       this.finalWordList.push(this.wordList[i][0])
     }
     this.finalWord = this.finalWordList.join('')
-    
+
     // WORD CHECK
 
+    if (this.secretWords.includes(this.finalWord)) {
+      let checked = false
 
-    if( this.secretWords.includes(this.finalWord) ){
-
-      let checked = false;
-
-      if(this.finalWord === 'LET' && checked === false){
-        this.add.dom(256, 120, 'h2', this.style, 'L');
-        this.add.dom(256, 172, 'h2', this.style, 'E');
-        this.add.dom(256, 224, 'h2', this.style, 'T');
+      if (this.finalWord === 'FEAR' && checked === false) {
+        this.add.dom(100, 150, 'h2', this.style, 'F')
+        this.add.dom(100, 202, 'h2', this.style, 'E')
+        this.add.dom(100, 254, 'h2', this.style, 'A')
+        this.add.dom(100, 306, 'h2', this.style, 'R')
         this.count--
-        this.secretWords.splice(2,1)
+        this.secretWords.splice(0, 1)
         checked = true
       }
 
+      if (this.finalWord === 'FEEL' && checked === false) {
+        this.add.dom(100, 150, 'h2', this.style, 'F')
+        this.add.dom(152, 150, 'h2', this.style, 'E')
+        this.add.dom(204, 150, 'h2', this.style, 'E')
+        this.add.dom(256, 150, 'h2', this.style, 'L')
+        this.count--
+        this.secretWords.splice(1, 1)
+        checked = true
+      }
+
+      if (this.finalWord === 'LET' && checked === false) {
+        this.add.dom(256, 150, 'h2', this.style, 'L')
+        this.add.dom(256, 202, 'h2', this.style, 'E')
+        this.add.dom(256, 254, 'h2', this.style, 'T')
+        this.count--
+        this.secretWords.splice(2, 1)
+        checked = true
+      }
+
+      if (this.finalWord === 'TEAR' && checked === false) {
+        this.add.dom(256, 254, 'h2', this.style, 'T')
+        this.add.dom(308, 254, 'h2', this.style, 'E')
+        this.add.dom(360, 254, 'h2', this.style, 'A')
+        this.add.dom(412, 254, 'h2', this.style, 'R')
+        this.count--
+        this.secretWords.splice(3, 1)
+        checked = true
+      }
+
+      if (this.finalWord === 'RED' && checked === false) {
+        this.add.dom(412, 254, 'h2', this.style, 'R')
+        this.add.dom(412, 306, 'h2', this.style, 'E')
+        this.add.dom(412, 358, 'h2', this.style, 'D')
+        this.count--
+        this.secretWords.splice(4, 1)
+        checked = true
+      }   
+
+    } else {
+      alert('wrong or already exist')
     }
-    else{
-      alert('wrong or already exist');
-    }
-
-
-
 
     // RESET DATA
     this.finalWordList = []
     this.finalWord = ''
     this.wordList = []
   })
-
-
 }
 
 function update() {
